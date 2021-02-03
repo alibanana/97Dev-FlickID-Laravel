e470a1e8e55f942117c4953c6db0cfbe40e0703e
Fernandha Dzaky <fernandhadzaky@hotmail.com>
2021-02-03 10:53:10 +0700

    update AppServiceProvider for debug
    The error was code for bug Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Flick Software
                 </div>
 
                 <div class="links">
