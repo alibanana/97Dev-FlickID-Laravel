bbbef59e9047ef5602d55403cdddfed24bbe0488
Alifio Rasyid <official.alifio@gmail.com>
2021-02-03 13:38:21 +0700

    Added migration file to create project_details table

---
+++ database/migrations/2021_02_03_063603_create_project_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 40);
+            $table->text('description');
+            $table->string('ilustration_file', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_details');
+    }
+}
