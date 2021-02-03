7c613203d3a3a851dafd7b5a8d21e017374cd682
Fernandha Dzaky <fernandhadzaky@hotmail.com>
2021-02-03 14:36:51 +0700

    trying navbar

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,15 @@
+@extends('client/layouts/main')
+
+@section('title', 'Flick Software Home Page')
+
+@section('container')
+
+<div style="margin-bottom:100px">
+    <p>atas</p>
+</div>
+
+
+<div style="margin-top:1000px">
+    <p>test</p>
+</div>
+@endsection
