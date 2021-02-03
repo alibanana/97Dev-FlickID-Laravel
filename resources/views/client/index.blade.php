dc3904b11efeb07d9af6bc5628b906039f5de5a9
Fernandha Dzaky <fernandhadzaky@hotmail.com>
2021-02-03 17:52:09 +0700

    top section for client landing page

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,15 +1,98 @@
-@extends('client/layouts/main')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('title', 'Flick Software Home Page')
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-@section('container')
+    <!-- INDEX CSS -->
+    <link rel="stylesheet"  type="text/css"  href="/css/client/index.css">
 
-<div style="margin-bottom:100px">
-    <p>atas</p>
-</div>
+    <!--load all fontawesome -->
+    <link href="/fontawesome/css/all.css" rel="stylesheet"> 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
 
+    
+    <title>Flick Software</title>
+    <style>
+      html{
+        width:100%;
+        height:100%;
+        margin:0;
+        padding:0;
+      }
+      body{
+        width:100%;
+        height:100%;
+        margin:0;
+        padding:0;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="row m-0" style="z-index:99;padding-top:30px">
+      <div class="col-md-12 p-0">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <!--<a class="navbar-brand" href="#">Navbar</a>-->
+            <p></p>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-<div style="margin-top:1000px">
-    <p>test</p>
-</div>
-@endsection
+              </ul>
+              <div class="" style="">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size:20px;font-family:HKGroteskBold">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#" style="margin-right:65px;color:black">Portofolio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" style="margin-right:65px;color:black;">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" style="margin-right:65px;color:black">The Team</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" style="margin-right:65px;color:black">Career</a>
+                  </li>
+                  <li class="nav-item" style="background-color:#ffff;margin-right:16px;padding:4px 70px;border-radius:10px">
+                    <a class="nav-link" href="#" style="color:#3F92D8;">Contact us!</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div class="row m-0">
+      <div class="col-md-6" style="padding-left:150px">
+        <img src="/assets/client/images/logoflick.png" class="img-fluid" style="width:60%;margin-top:100px"  alt="">
+        <p style="font-size:68  px;margin-top:30px;font-family:HKGroteskBlack">Build and grow<br>together with us</p>
+        <p style="font-size:23px;margin-bottom:60px;font-family:HKGroteskBold">We develop softwares from <b>mobile apps, <br> web apps, even advanced app-based services</b> </p>
+        <a style="font-size:23px;background-color:#3F92D8;color:#FFFFFF;padding:20px 40px;text-decoration:none;border-radius:15px;font-family:HKGroteskBold" href="#">Discover our works</a>
+      </div>
+
+      <div class="col-md-6 p-0" style="text-align:right">
+        <img src="/assets/client/images/Group_346.png" class="img-fluid" style="margin-top:-160px;" alt="">
+      </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- resources/views/client/layouts/main.blade.php
+++ resources/views/client/layouts/main.blade.php
@@ -14,32 +14,12 @@
     <title>@yield('title')</title>
   </head>
   <body>
-    <p style="margin-bottom:30px">test</p>
     <!-- NAVBAR DESKTOP -->
-    <nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light navbar-fixed-top">
-      <div class="container">
-          <a class="navbar-brand" href="#">Brand</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar1">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbar1">
-              <ul class="navbar-nav">
-                  <li class="nav-item active">
-                      <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
-                  </li>
-              </ul>
-              <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active">
-                      <a class="nav-link" href="#">Link</a>
-                  </li>
-              </ul>
-          </div>
-      </div>
-    </nav>
+    
     <!-- END OF NAVBAR DESKTOP -->
+
+    
+    <img src="/assets/client/images/Group_346.png" style="float:right;width:30%" alt="">
     
     @yield('container')
 
