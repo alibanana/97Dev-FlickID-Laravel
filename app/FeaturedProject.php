86d1e3c3946cf625e14edc10d084aee55b1e4d5a
Alifio Rasyid <official.alifio@gmail.com>
2021-02-08 23:07:06 +0700

    Update FeaturedProject model.
    Update the FeaturedProject model as according to the updates for the corresponding table.

--- app/FeaturedProject.php
+++ app/FeaturedProject.php
@@ -9,7 +9,9 @@ class FeaturedProject extends Model
     protected $table = 'featured_projects';
     
     protected $fillable = [
-        'position', 'logo_file', 'ilustration_file', 'color_code', 'project_id'
+        'position', 'logo_file', 'ilustration_file', 'filter_invert',
+        'filter_sepia', 'filter_saturate', 'filter_hue_rotate', 'filter_brightness',
+        'filter_contrast', 'project_id'
     ];
 
     // Get the project a particular featured-project is belonging to.
