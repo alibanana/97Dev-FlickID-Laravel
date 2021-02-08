1972d6977ee2de1207c64e5607bc1a61b4c7d444
Alifio Rasyid <official.alifio@gmail.com>
2021-02-08 21:13:10 +0700

    Update admin's page main layout

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -80,10 +80,10 @@
       </div>
     </div>
     <!-- END OF POPUP -->
-<nav class="navbar navbar-expand-lg navbar-light bg-white m-0 mt-3" style="padding:0px 100px">
+<nav class="navbar navbar-expand-lg navbar-light bg-white m-0 mt-3 mb-3" style="padding:0px 100px">
 <div class="container">
   <div class="col-4 ">
-    <a class="navbar-brand pl-1" href="/"><img src="assets/logoflick.png" alt=""></a>
+    <a class="navbar-brand pl-1" href="/"><img style="width: 250px" src="{{ asset('assets/client/images/Flick Software Logo Blue.png') }}" alt="Flick Software"></a>
   </div>
     <div class="col-8 row " id="navbarNav" >
       <ul class="justify-content-between nav justify-content-start">
