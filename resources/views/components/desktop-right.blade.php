c8d26d6dfb2a1ee7f92f10701008df586959c63d
fernndzaky <fernandhadzaky@hotmail.com>
2021-02-13 23:41:51 +0700

    index mobile responsive

--- resources/views/components/desktop-right.blade.php
+++ resources/views/components/desktop-right.blade.php
@@ -1,8 +1,8 @@
 @if($flag == 2)
-<div class="col-md-5 p-0" style="margin-top:7vw;z-index:99">
+<div class="col-md-5 p-0 DesktopView" style="margin-top:7vw;z-index:99">
     <div style="padding-left:15%">
         @if (Request::path() == '/')
-        <p style="font-size:4vw;font-family:HKGroteskBlack;line-height:1.2"> <b>Explore more on <br> our fine works</b></p>
+        <p style="font-size:4.5vw;font-family:HKGroteskBlack;line-height:1.2"> <b>Explore more on <br> our fine works</b></p>
         <div>
             <a href="{{ url()->route('portfolio.index') }}" style="font-size:1.8vw;font-family:HKGroteskBold;text-decoration:none;color:#3F92D8">See more work <i style="font-size:1.8vw;margin-left:5px" class="fas fa-long-arrow-alt-right"></i></a>
         </div>
@@ -13,23 +13,23 @@
     </div>
 </div>
 @else
-<div class="col-md-5 p-0"></div>
+<div class="col-md-5 p-0 DesktopView"></div>
 @endif
 
-<div class="col-md-7 p-0" style="margin-top:-21vw">
+<div class="col-md-7 p-0 marginTopPortofolio" style="margin-top:-21vw">
     <div class="containerImage">
         <div style="text-align:right">   
             <img src="/assets/client/images/Hex_Right_Path.svg" class="img-fluid" alt="Snow" style="width:93%;filter: invert({{ $featuredProject->filter_invert }}%) sepia({{ $featuredProject->filter_sepia }}%) saturate({{ $featuredProject->filter_saturate }}%) hue-rotate({{ $featuredProject->filter_hue_rotate }}deg) brightness({{ $featuredProject->filter_brightness }}%) contrast({{ $featuredProject->filter_contrast }}%);">
         </div>
         <div class="top-right">
-            <div style="text-align:right !important;padding-left:15%;padding-top:7%;padding-right:3.1vw">
-                <img src="{{ asset($featuredProject->ilustration_file) }}" class="img-fluid" style="width:28vw" alt="">
-                <div style="margin-top:40px">
-                    <img src="{{ asset($featuredProject->logo_file) }}" class="img-fluid" style="width:11vw"  alt="">
-                    <div style="padding-left:12%">
+            <div style="text-align:right !important;padding-left:15%;padding-top:7%;padding-right:3.1vw" class="portofolioTextAndImage">
+                <img id="illustrationDesktop" src="{{ asset($featuredProject->ilustration_file) }}" class="img-fluid" style="width:28vw" alt="">
+                <div style="margin-top:40px"  id="desktopMarginTop">
+                    <img id="logo" src="{{ asset($featuredProject->logo_file) }}" class="img-fluid" style="width:11vw"  alt="">
+                    <div style="padding-left:20%">
                         <p style="font-family:HKGroteskRegular;font-size:1.5vw;margin-top:10px;color:black">{{ $featuredProject->project->description }} <span style="font-family:HKGroteskBold">{{ $featuredProject->project->sub_description }}</span></p>
                     </div>
-                    <div style="padding-top:15px">
+                    <div style="padding-top:15px" id="portofolioTextPadding">
                         <a href="{{ url('portfolio').'/'.$featuredProject->project->id }}" style="font-size:1.5vw;font-family:HKGroteskBold;text-decoration:none;color:#3F92D8;">See our work <i style="font-size:1.5vw;margin-left:5px" class="fas fa-long-arrow-alt-right"></i></a>
                     </div>
                 </div>
--- resources/views/components/mobile-left.blade.php
+++ resources/views/components/mobile-left.blade.php
@@ -1,22 +1,22 @@
 @if($flag == 1)
 <div class="col-md-7 p-0" >
 @else
-<div class="col-md-7 p-0" style="margin-top:-24vw" >
+<div class="col-md-7 p-0 marginTopPortofolio" style="margin-top:-24vw" >
 @endif
     <div class="containerImage">
         <div style="text-align:left">   
             <img src="/assets/client/images/Hex_Left_Path.svg" class="img-fluid" alt="Snow" style="width:93%; filter: invert({{ $featuredProject->filter_invert }}%) sepia({{ $featuredProject->filter_sepia }}%) saturate({{ $featuredProject->filter_saturate }}%) hue-rotate({{ $featuredProject->filter_hue_rotate }}deg) brightness({{ $featuredProject->filter_brightness }}%) contrast({{ $featuredProject->filter_contrast }}%);">
         </div>
         <div class="top-left">
-            <div style="text-align:left !important;padding-left:15%;padding-top:18%">
+            <div style="text-align:left !important;padding-left:15%;padding-top:18%" class="portofolioTextAndImage">
             <div style="display:flex;align-items: flex-start">
-                <img src="{{ asset($featuredProject->ilustration_file) }}" class="img-fluid" style="width:16vw" alt="">
+                <img id="illustration" src="{{ asset($featuredProject->ilustration_file) }}" class="img-fluid" style="width:16vw" alt="">
                 <div style="margin-left:30px">
-                    <img src="{{ asset($featuredProject->logo_file) }}" class="img-fluid" style="width:11vw"  alt="">
-                <div style="width:22vw">
+                    <img id="logo" src="{{ asset($featuredProject->logo_file) }}" class="img-fluid" style="width:11vw"  alt="">
+                <div style="width:22vw" id="portofolioTextWidth">
                     <p style="font-family:HKGroteskRegular;font-size:1.5vw;margin-top:3vw;color:black">{{ $featuredProject->project->description }} <span style="font-family:HKGroteskBold">{{ $featuredProject->project->sub_description }}</span></p>
                 </div>
-                <div style="padding-top:15px">
+                <div style="padding-top:15px" id="portofolioTextPadding">
                     <a href="{{ url('portfolio').'/'.$featuredProject->project->id }}" style="font-size:1.5vw;font-family:HKGroteskBold;text-decoration:none;color:#3F92D8;">See our work <i style="font-size:1.5vw;margin-left:5px" class="fas fa-long-arrow-alt-right"></i></a>
                 </div>
                 </div>
@@ -27,7 +27,7 @@
 </div>
 
 @if($flag == 1)
-<div class="col-md-5 p-0">
+<div class="col-md-5 p-0 DesktopView">
     <div class="containerImage">
         <div style="">   
         <img src="/assets/client/images/Hex_-_What_weve_build.svg" class="img-fluid" alt="Snow" style="width:65%;margin-top:-40px;margin-left:-20vw">
