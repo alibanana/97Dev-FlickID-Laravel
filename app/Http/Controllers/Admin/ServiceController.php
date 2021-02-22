5d5b7ec3347f9f476e95de4ae480aa50031e20a0
Alifio Rasyid <official.alifio@gmail.com>
2021-02-23 01:16:00 +0700

    Added Admin/ServiceController
    Controls the CRUD features for ProjectTypes.

---
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManagerStatic as ImageManager;
+
+use App\ProjectType;
+
+class ServiceController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        $project_types = ProjectType::all();
+
+        return view('admin/services', compact('project_types'));
+    }
+
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'ilustration_file' => 'required|mimes:jpeg,jpg,png,svg',
+            'type' => 'required|max:20',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/admin/service#add-service')->withErrors($validator)->withInput();
+        }
+
+        $project_type = new ProjectType;
+        $project_type->type = $input['type'];
+        $project_type->description = $input['description'];
+        $project_type->ilustration_file = $this->storeImage($request->file('ilustration_file'), 'ilustrations/', 'ilustration');
+        $project_type->save();
+
+        return redirect()->route('admin.service.index')->with('success', 'A new Service has been added to the database!');
+    }
+
+    private function storeImage($image, $foldername, $image_type){
+        $ext = strtolower($image->getClientOriginalExtension());
+        $destinationPath = 'storage/images/'.$foldername;
+
+        if (!File::isDirectory($destinationPath)){
+            File::makeDirectory($destinationPath, 0777, true, true);
+        }
+
+        while(true){
+            $newName = $image_type.'_'.rand(100000,PHP_INT_MAX).'.'.$ext;
+            if (!file_exists($destinationPath.$newName)){
+                break;
+            }
+        }
+
+        if ($ext == 'jpg' || $ext == 'jpeg') {
+            $img_created = ImageManager::make($image->getRealpath());
+            $img_created->orientate();
+            $img_created->save($destinationPath.$newName, 75);
+        } else {
+            $image->move($destinationPath, $newName);
+        }
+
+        return $destinationPath.$newName;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+
+        Validator::make($input, [
+            'ilustration_file' => 'mimes:jpeg,jpg,png,svg',
+            'type' => 'required|max:20',
+            'description' => 'required'
+        ])->validate();
+
+        $project_type = ProjectType::findorfail($id);
+        $project_type->type = $input['type'];
+        $project_type->description = $input['description'];
+
+        if ($request->has('ilustration_file')) {
+            unlink($project_type->ilustration_file);
+            $project_type->ilustration_file = $this->storeImage($request->file('ilustration_file'), 'ilustrations/', 'ilustration');
+        }
+
+        $project_type->save();
+
+        return redirect()->route('admin.service.index')->with('success', 'Service has been updated!');
+    }
+
+    public function destroy($id)
+    {
+        $project_type = ProjectType::findorfail($id);
+        
+        unlink($project_type->ilustration_file);
+        $project_type->delete();
+
+        return redirect()->route('admin.service.index')->with('success', 'Service has been deleted!');
+    }
+}
--- resources/views/admin/services.blade.php
+++ resources/views/admin/services.blade.php
@@ -9,49 +9,48 @@
 <!-- START OF POPUP -->
 <div id="add-service" class="overlay">
     <div class="popup">
-    <a class="close" href="#" >&times;</a>
-    
-    <div class="content" style="padding:20px">
-      <h1>Add New Service</h1>
-      <div class="col-md-12">
-        <form action="" method="POST">
-          @csrf
-          <div class="form-group">
-              <label for="inputName">Type</label>
-              <input name="type" type="text" class="form-control" placeholder="e.g. Mobile Application">
-          </div>
-          @error('type')
-          <span class="invalid-feedback" role="alert" style="display: block !important;">
+      <a class="close" href="#" >&times;</a>
+      <div class="content" style="padding:20px">
+        <h1>Add New Service</h1>
+        <div class="col-md-12">
+          <form enctype="multipart/form-data" action="{{ route('admin.service.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="inputName">Type</label>
+                <input name="type" value="{{ old('type') }}" type="text" class="form-control" placeholder="e.g. Mobile Application">
+            </div>
+            @error('type')
+            <span class="invalid-feedback" role="alert" style="display: block !important;">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+            <div class="form-group mt-3">
+                <label for="description">Enter Description</label>
+                <input name="description" value="{{ old('description') }}" type="text" class="form-control" id="" placeholder="Enter Description">
+            </div>
+            @error('description')
+            <span class="invalid-feedback" role="alert" style="display: block !important;">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+            <h5 style="color:#145CA8;margin-top:30px">Picture</h5>
+            <input type="file" id="image" name="ilustration_file" accept=".jpg,.jpeg,.png" hidden/>
+            <label id="uploadButton" for="image">Choose File</label>
+            @error('ilustration_file')
+            <span class="invalid-feedback" role="alert" style="display: block !important;">
               <strong>{{ $message }}</strong>
-          </span>
-          @enderror
-          <div class="form-group mt-3">
-              <label for="description">Enter Description</label>
-              <input name="description" type="text" class="form-control" id="" placeholder="Enter Description">
-          </div>
-          @error('description')
-          <span class="invalid-feedback" role="alert" style="display: block !important;">
-              <strong>{{ $message }}</strong>
-          </span>
-          @enderror
-          <h5 style="color:#145CA8;margin-top:30px">Picture</h5>
-        <input type="file" id="image" name="photo_file" accept=".jpg,.jpeg,.png" hidden/>
-        <label id="uploadButton" for="image">Choose File</label>
-        @error('photo_file')
-        <span class="invalid-feedback" role="alert" style="display: block !important;">
-        <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-        <!-- START OF UPLOADED IMAGE -->
-        <div id="image_preview" class="row m-0">
+            </span>
+            @enderror
+            <!-- START OF UPLOADED IMAGE -->
+            <div id="image_preview" class="row m-0">
+            </div>
+            <!-- END OF UPLOADED IMAGE -->
+            <div style ="display:flex; justify-content: flex-end; ">
+              <button type="submit" class="btn btn-warning btn-sm">Add Service</button>
+            </div>
+          </form>
         </div>
-        <!-- END OF UPLOADED IMAGE -->
-        <div style ="display:flex; justify-content: flex-end; ">
-          <button type="submit" class="btn btn-warning btn-sm">Add Service</button>
-        </div>
-        </form>
       </div>
-    </div>
     </div>
 </div>
 <!-- END OF POPUP -->
@@ -81,50 +80,50 @@
     <thead>
       <tr>
         <th scope="col">No</th>
-        <th scope="col">Title</th>
+        <th scope="col">Title & Image</th>
         <th scope="col">Description</th>
-        <th scope="col">Image</th>
+        <th scope="col">Upload Image</th>
         <th scope="col">Action</th>
       </tr>
     </thead>
     <tbody style="font-family:HKGroteskRegular !important">
+      @foreach ($project_types as $project_type)
         <tr>
-          <td>1</td>
-          <form>
-          <td>
-            <input type="text" value="Mobile Apps" name="type">
-          </td>
-          <td>
-          <textarea type="text-area" cols="60" rows="2" class="form-control" id="description" name="description" aria-describedby="descriptionHelp" style="width:70%">Creating stable and easy to use application for both Android and iOS</textarea>
-
-          
-          </td>
-          <td>
-            <label style="font-family: HKGroteskRegular">Current Pic: </label>
-            <img style="object-fit: cover; width: 100px; margin-top: 10px" src="/assets/seeder/project-type-websites-icon.svg" alt="">
-            <div class="form-group" style="margin-top: 20px">
-              <label for="photo_file">Upload New File</label>
-              <input type="file" class="form-control-file" id="photo_file" name="photo_file" style="width: 100%; margin-top: 10px">
-            </div>
-          </td>
-          <td>
-            <button type="submit" class="btn btn-secondary" style="margin-right: 10px" onclick='return confirm("Are you sure you want to Update this service?")'>Update</button>
-            </form>
-            <form action="" method="post" style="display: inline-block">
+          <td>{{ $loop->iteration }}</td>
+          <form enctype="multipart/form-data" id="update-project-type-{{ $loop->index }}" method="POST" action="{{ route('admin.service.update', $project_type->id) }}">
+            @csrf
+            @method('PUT')
+            <td style="width: 20%">
+              <input type="text" value="{{ $project_type->type }}" name="type"><br>
+              <label style="font-family: HKGroteskRegular">Current Pic:</label>
+              <img style="object-fit: cover; width: 100px; margin-top: 10px" src="{{ asset($project_type->ilustration_file) }}" alt="">
+            </td>
+            <td>
+              <textarea type="text-area" cols="60" rows="3" class="form-control" id="description" name="description" style="width: 80%" aria-describedby="descriptionHelp">{{ $project_type->description }}</textarea>
+            </td>
+            <td style="width: 15%">
+              <div class="form-group" style="">
+                <label for="ilustration_file">Upload New File</label>
+                <input type="file" class="form-control-file" id="ilustration_file" name="ilustration_file" style="margin-top: 10px">
+              </div>
+            </td>
+          </form>
+          <td style="width: 15%">
+            <button type="submit" form="update-project-type-{{ $loop->index }}" class="btn btn-secondary" style="margin-right: 10px" onclick='return confirm("Are you sure you want to Update this service?")'>Update</button>
+            <form action="{{ route('admin.service.destroy', $project_type->id) }}" method="post" style="display: inline-block">
               @csrf
               @method('DELETE')
-              <button class="btn btn-danger" type="submit" onclick='return confirm("Are you sure you want to Delete this Service? By Deleting this service, you will delete all project")'>Delete</button>
+              <button class="btn btn-danger" type="submit" onclick='return confirm("Deleting this Service will delete all corresponding Projects.\r\nAre you sure you still want to delete this Service?")'>Delete</button>
             </form>
           </td>
         </tr>
+      @endforeach
     </tbody>
   </table>
 </div>
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script>
-     //your script here
-
 // START OF IMAGE PREVIEW AND DELETE
 $(document).ready(function() {
   if (window.File && window.FileList && window.FileReader) {
@@ -163,6 +162,5 @@ $(document).ready(function() {
   }
 });
 // END OF IMAGE PREVIEW AND DELETE
-
 </script>
 @endsection
