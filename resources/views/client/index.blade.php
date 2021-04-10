d2620d76354ae7f8ba89b9b7d18d8c62bac7ecd3
fernndzaky <fernandhadzaky@hotmail.com>
2021-04-10 10:56:38 +0700

    add google tag

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -315,6 +315,17 @@
      
 
     </style>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167109367-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-167109367-1');
+    </script>
+
   </head>
   <body >
     <!-- START OF NAVBAR -->
